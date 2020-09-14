5a47b741f39202feb408c037bf450eb8334fc998
Harui1537 <dbgkgygod123@naver.com>
2020-09-14 13:20:49 +0900

    Add files via upload

---
+++ 2020-09-14연습/Rectangle.java
@@ -0,0 +1,27 @@
+
+/**
+ * 여기에 Rectangle 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class Rectangle
+{
+    public Rectangle()
+    {
+        // 인스턴스 변수의 초기화
+        x = 0;
+    }
+
+    /**
+     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 
+     * @param  y   메소드의 예제 매개변수
+     * @return     x 더하기 y 
+     */
+    public int sampleMethod(int y)
+    {
+        // 여기에 코드를 작성하십시오.
+        return x + y;
+    }
+}
